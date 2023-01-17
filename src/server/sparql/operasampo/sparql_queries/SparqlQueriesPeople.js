8c572181f676b2ee606f9fe2175e0a2ef7b5757e
Annastiina Ahola <anna.s.ahola@gmail.com>
2023-01-17 17:45:58 +0200

    Add properties to people perspective

--- src/server/sparql/operasampo/sparql_queries/SparqlQueriesPeople.js
+++ src/server/sparql/operasampo/sparql_queries/SparqlQueriesPeople.js
@@ -9,4 +9,49 @@ export const personProperties = `
       BIND(?id as ?uri__dataProviderUrl)
       BIND(?id as ?uri__prefLabel)
     }
+    UNION
+    {
+      ?id foaf:firstName ?firstName .
+    }
+    UNION
+    {
+      ?id foaf:surname ?lastName .
+    }
+    UNION
+    {
+      ?id scop:role ?role .
+    }
+    UNION
+    {
+      ?id scop:nationality ?nationality .
+    }
+    UNION
+    {
+      ?id scop:language ?language .
+    }
+    UNION
+    {
+      ?id scop:dateOfBirth ?dateOfBirth .
+    }
+    UNION
+    {
+      ?id scop:placeOfBirth ?placeOfBirth .
+    }
+    UNION
+    {
+      ?id scop:dateOfDeath ?dateOfDeath .
+    }
+    UNION
+    {
+      ?id scop:placeOfDeath ?placeOfDeath .
+    }
+    UNION
+    {
+      ?id scop:additionalInfo ?additionalInfo .
+      FILTER(LANG(?additionalInfo) = 'fi')
+    }
+    UNION
+    {
+      ?id scop:editorNotes ?editorNotes .
+    }
 `
