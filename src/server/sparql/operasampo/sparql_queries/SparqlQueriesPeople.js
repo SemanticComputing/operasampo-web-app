b20c44a7954a3f3adc48bb2d82259a9f84f97f09
Annastiina Ahola <anna.s.ahola@gmail.com>
2023-03-01 11:48:17 +0200

    Add role information for people

--- src/server/sparql/operasampo/sparql_queries/SparqlQueriesPeople.js
+++ src/server/sparql/operasampo/sparql_queries/SparqlQueriesPeople.js
@@ -54,4 +54,36 @@ export const personProperties = `
     {
       ?id scop:editorNotes ?editorNotes .
     }
+    UNION
+    {
+      ?id ^scop:actor ?performanceRole .
+      ?performanceRole a scop:PerformanceRole ;
+                      scop:compositionRole ?roleCharacter__id .
+      ?roleCharacter__id skos:prefLabel ?prefLabel_ ;
+                        scop:composition ?composition .
+      FILTER(LANG(?prefLabel_) = 'fi')
+      ?composition skos:prefLabel ?comp_pref_fi .
+      FILTER(LANG(?comp_pref_fi) = 'fi')
+      BIND(COALESCE(?prefLabel_, ?roleCharacter__id) as ?rc_pref)
+      BIND(CONCAT(?rc_pref, " (", ?comp_pref_fi, ")") as ?roleCharacter__prefLabel)
+      BIND(CONCAT("/roles/page/", REPLACE(STR(?roleCharacter__id), "^.*\\\\/(.+)", "$1")) AS ?roleCharacter__dataProviderUrl)
+    }
+`
+
+export const personRolesQuery = `
+  SELECT ?category ?prefLabel (COUNT(DISTINCT ?performanceRole) as ?instanceCount)
+  WHERE {
+    BIND(<ID> as ?person)
+    ?person a scop:Person .
+    ?performanceRole a scop:PerformanceRole ;
+                    scop:actor ?person .
+    ?performanceRole scop:compositionRole ?category .
+    OPTIONAL {
+      ?category skos:prefLabel ?prefLabel_ .
+      FILTER(LANG(?prefLabel_) = 'fi')
+    }
+    BIND(COALESCE(?prefLabel_, ?category) as ?prefLabel)
+  }
+  GROUP BY ?category ?prefLabel
+  ORDER BY DESC(?instanceCount)
 `
