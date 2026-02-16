dda75bb121a591de3345ebb1b580f115162d631f
Annastiina Ahola <anna.s.ahola@gmail.com>
2026-02-16 15:36:48 +0200

    Add plugins and stylesheets to ImageGallerySRL

--- client/src/components/main_layout/ImageGallerySRL.js
+++ client/src/components/main_layout/ImageGallerySRL.js
@@ -2,6 +2,11 @@ import React from 'react'
 import { styled } from '@mui/material/styles'
 import Button from '@mui/material/Button'
 import Lightbox from 'yet-another-react-lightbox'
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails"
+import Captions from "yet-another-react-lightbox/plugins/captions"
+import "yet-another-react-lightbox/styles.css"
+import "yet-another-react-lightbox/plugins/thumbnails.css"
+import "yet-another-react-lightbox/plugins/captions.css"
 
 const ImgPreviewImage = styled('img')({
   border: '1px solid lightgray'
@@ -15,11 +20,13 @@ const ImageGallerySRL = props => {
 
   const [open, setOpen] = React.useState(false)
 
+  const thumbnailsRef = React.useRef(null)
+
   const images = data.map(item => {
     return {
       src: item.url,
       thumbnail: item.url,
-      caption: item.description
+      description: item.description
     }
   })
   return (
@@ -31,7 +38,20 @@ const ImageGallerySRL = props => {
           alt='preview image'
         />
       </Button>
-      <Lightbox open={open} close={() => setOpen(false)} slides={images} />
+      <Lightbox 
+        open={open}
+        close={() => setOpen(false)} 
+        slides={images} 
+        plugins={[Thumbnails, Captions]}
+        thumbnails={{ ref: thumbnailsRef }}
+        on={{
+          click: () => {
+            (thumbnailsRef.current?.visible
+              ? thumbnailsRef.current?.hide
+              : thumbnailsRef.current?.show)?.()
+          },
+        }}
+      />
     </>
   )
 }
